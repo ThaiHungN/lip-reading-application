39da34a966b38e888906338b65bd22bff6743033
Nguyễn Thái Hùng <v.hungnt78@vinai.io>
2024-02-10 19:25:12 +0700

    update

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def upload():
         
         # video -> subtitle_content
         result = execute_model_infer()
-        print("debug 4", result)
+        print("debug 4", result, result.result, result.result.split("hyp: "))
         subtitle = result.result.split("hyp: ")[1]
         print("debug 5", subtitle)
         # Sample WebVTT content
